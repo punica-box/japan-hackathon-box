fea1de0d651658b0f68e1e6cdab419b45754d77a
futabooo <takahiro.futagawa@gmail.com>
2018-12-09 11:34:57 +0900

    Add top screen

--- #14-ギャンブル/src/index.tsx
+++ #14-ギャンブル/src/index.tsx
@@ -1,10 +1,19 @@
 import * as Ontology from "ontology-dapi";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
-import App from "./App";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Top } from "./top";
+
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
+
+const App: React.SFC<{}> = () => (
+  <Router>
+    <>
+      <Route path="/" exact={true} component={Top} />
+    </>
+  </Router>
+);
 
 Ontology.client.registerClient({});
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
--- #14-ギャンブル/src/top.tsx
+++ #14-ギャンブル/src/top.tsx
@@ -1,44 +1,21 @@
-import * as React from 'react';
-import { RouterProps } from 'react-router';
+import * as React from "react";
+import { RouterProps } from "react-router";
 
-export const Top: React.SFC<RouterProps> = (props) => {
-    function onNetwork() {
-      props.history.push('/network');
-    }
-  
-    function onAsset() {
-      props.history.push('/asset');
-    }
-  
-    function onOep4() {
-      props.history.push('/oep4');
-    }
-  
-    function onSmartContract() {
-      props.history.push('/smart-contract');
-    }
-  
-    function onProvider() {
-      props.history.push('/provider');
-    }
-  
-    function onMessage() {
-      props.history.push('/message');
-    }
-  
-    return (
-      <div>
-        <button onClick={onProvider}>Provider</button>
-        <hr />
-        <button onClick={onNetwork}>Network</button>
-        <hr />
-        <button onClick={onMessage}>Messaage</button>
-        <hr />
-        <button onClick={onAsset}>Asset</button>
-        <hr />
-        <button onClick={onOep4}>Oep4</button>
-        <hr />
-        <button onClick={onSmartContract}>Smart contract</button>
-      </div>
-    );
-  };
+export const Top: React.SFC<RouterProps> = props => {
+  function bePlayer() {
+    props.history.push("/player-gamelist");
+  }
+
+  function beDealer() {
+    props.history.push("/deal-list");
+  }
+
+  return (
+    <div>
+      <button onClick={bePlayer}>bePlayer</button>
+      <hr />
+      <button onClick={beDealer}>beDealer</button>
+      <hr />
+    </div>
+  );
+};
